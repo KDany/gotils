e114e14e5ce8afbbd7b9192f7e2b408e764247f6
Daniel Kriston <k.danihun@windowslive.com>
2025-07-30 16:46:48 +0200

    feat: add main entry

---
+++ gotils.go
@@ -0,0 +1,21 @@
+package gotils
+
+import (
+	"gotils/logger"
+)
+
+const Test = "This is a test constant for gotils package"
+
+func main() {
+	// Example usage of Logger interface
+	logger.SetConfig(logger.Config{
+		Announcer:   "MyApp",
+		TimeFormat:  "2006-01-02 15:04:05",
+		LogPrefix:   "[INFO]",
+		WarnPrefix:  "[WARN]",
+		ErrorPrefix: "[ERROR]",
+		ExitOnError: true,
+	})
+	logger.Log.Info("This is an info message")
+	logger.Log.Error("This is an error message")
+}
